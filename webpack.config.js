7c27a435fc441c4e08aa09765e17b4ef362c8aa7
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 17:52:20 -0500

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+'use strict'
+const path              = require('path');
+const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const BUILD_DIR         = path.resolve(__dirname, 'dist');
+const APP_DIR           = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: '/js/[name].js',
+  },
+  cache: true,
+  debug: true,
+  devtool: 'eval-source-map',
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'reAsk',
+      xhtml: true,
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: 'root-container',
+      scripts: [
+        "/socket.io/socket.io.js"
+      ]
+    }),
+    new ExtractTextPlugin('/css/[name].css', {
+      allChunks: true
+    })
+  ],
+
+  module : {
+    include: path.join(__dirname, 'src'),
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      // {
+      //   test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+      //   loader: 'file-loader',
+      // },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      {
+        test: /\.gif$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      { test: /\.jpg$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=/fonts/[name].[ext]'
+      }
+    ]
+  }
+};
