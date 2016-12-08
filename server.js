d0c98cd6179d6b055f0240b17ecda9d8d95766c2
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 17:31:29 -0500

    server running

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// requiring express
+const express = require('express');
+// intance of morgan for server logs
+const logger = require('morgan');
+
+// initializing an instance of express
+const app = express();
+// selecting options for morgan
+app.use(logger('dev'));
+
+// determining port config, heroku considerations
+const PORT = process.argv[2] || process.env.PORT || 3000;
+
+// app.use(express.static(path.join(__dirname, 'dist')));
+
+// setting the server to listen on PORT
+app.listen(PORT, () => console.log(`proyecturune on ${PORT}`))
