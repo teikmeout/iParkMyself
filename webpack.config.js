7fdc9b1a03c43dadfbad1c4c8bd9b3a5a54da0f7
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-09 10:48:22 -0500

    added correct config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 'use strict'
-const path              = require('path');
+// basic web pack require
 const webpack           = require('webpack');
+// path for correct file pointing
+const path              = require('path');
+// weback plugin for HTML construction
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// I don't know what this does
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// const htmlTemplate = require('html-webpack-template');
 
 const BUILD_DIR         = path.resolve(__dirname, 'dist');
 const APP_DIR           = path.resolve(__dirname, 'src');
 
-module.exports = {
+const config = {
   entry: `${APP_DIR}/index.jsx`,
   output: {
     path: BUILD_DIR,
@@ -21,6 +27,12 @@ module.exports = {
     reasons: true
   },
   plugins: [
+    // don't know if I need this or not
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    //   },
+    // }),
     new HtmlWebpackPlugin({
       title: 'reAsk',
       xhtml: true,
@@ -39,25 +51,15 @@ module.exports = {
   module : {
     include: path.join(__dirname, 'src'),
     loaders: [
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-      },
-      // {
-      //   test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-      //   loader: 'file-loader',
-      // },
-      {
-        test: /\.svg$/,
-        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
-      },
-      {
-        test: /\.gif$/,
-        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
-      },
-      { test: /\.jpg$/,
-        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
-      },
+      // I'm pretty sure this is the CSS styling loader
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+      // checking for png gif and jpg files
+      { test: /\.(png|gif|jpg)$/, loader: 'file-loader?name=/images/[name].[ext]' },
+      // i'm not using svg's but Let's put this here
+      { test: /\.svg$/, loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'},
+      // { test: /\.gif$/, loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'},
+      // { test: /\.jpg$/, loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'},
+      // testing for JS and JSX files
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -70,3 +72,29 @@ module.exports = {
     ]
   }
 };
+
+// this part is added from @jasonseminara web pack
+// it's a dev environment check
+if (process.env &&
+  process.env.NODE_ENV &&
+  process.env.NODE_ENV === 'production') {
+  const prodPlugins = [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new webpack.optimize.CommonsChunkPlugin('/js/common.js'),
+  ];
+
+  config.plugins = config.plugins.concat(prodPlugins);
+
+  config.cache = false;
+  config.debug = false;
+  config.devtool = undefined;
+}
+
+module.exports = config;
